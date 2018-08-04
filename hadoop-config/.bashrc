7b629479067e264d53dec779d745765c18930b79
cupcake <jbw@users.noreply.github.com>
2018-08-04 11:22:51 +0100

    convert to shell scripts

---
+++ hadoop-config/.bashrc
@@ -0,0 +1,17 @@
+
+# Make sure environment variables are loaded
+export HADOOP_HOME=/usr/local/hadoop 
+export PATH=$PATH:/usr/local/hadoop/bin:/usr/local/hadoop/sbin 
+export JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/
+
+
+export HADOOP_HOME=/usr/local/hadoop 
+
+export HDFS_NAMENODE_USER="pi"
+export HDFS_DATANODE_USER="pi"
+export HDFS_SECONDARYNAMENODE_USER="pi"
+export YARN_RESOURCEMANAGER_USER="pi"
+export YARN_NODEMANAGER_USER="pi"
+
+export PATH=$PATH:/usr/local/hadoop/bin:/usr/local/hadoop/sbin 
+
--- install.sh
+++ install.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
+sudo apt-get update -y
 
-sudo apt-get update
+sudo apt-get install -y oracle-java8-jdk net-tools openssh-server wget vim git
 
-sudo apt-get install -y openssh-server wget
+# Install hadoop binary
+wget https://github.com/jbw/build-hadoop/releases/download/3.0.3-armv7l/hadoop-3.0.3.tar.gz && tar -xzvf hadoop-3.0.3.tar.gz && mv hadoop-3.0.3 /usr/local/hadoop
 
-sudo apt-get install -y software-properties-common debconf-utils net-tools vim git
+cp ./hadoop-config/.bashrc ~/.bashrc && source ~/.bashrc
 
-sudo apt-get install libtool
+mkdir -p ~/hdfs/namenode && \
+mkdir -p ~/hdfs/datanode && \
+mkdir $HADOOP_HOME/logs
 
-sudo apt-get install oracle-java8-jdk
+cp ./hadoop-config/.bashrc ~/.bashrc && source ~/.bashrc
+cp ./hadoop-config/ssh_config ~/.ssh/config && \
+cp ./hadoop-config/hadoop-env.sh /usr/local/hadoop/etc/hadoop/hadoop-env.sh && \
+cp ./hadoop-config/hdfs-site.xml $HADOOP_HOME/etc/hadoop/hdfs-site.xml && \
+cp ./hadoop-config/core-site.xml $HADOOP_HOME/etc/hadoop/core-site.xml && \
+cp ./hadoop-config/mapred-site.xml $HADOOP_HOME/etc/hadoop/mapred-site.xml && \
+cp ./hadoop-config/yarn-site.xml $HADOOP_HOME/etc/hadoop/yarn-site.xml && \
+cp ./hadoop-config/slaves $HADOOP_HOME/etc/hadoop/slaves && \
+cp ./hadoop-config/start-hadoop.sh ~/start-hadoop.sh && \
+cp ./hadoop-config/start-slaves.sh ~/start-slaves.sh && \
 
-wget https://github.com/jbw/build-hadoop/releases/download/3.0.3-armv7l/hadoop-3.0.3.tar.gz
-tar -xzvf hadoop-3.0.3.tar.gz
-mv hadoop-3.0.3 /usr/local/hadoop
+chmod +x ~/start-hadoop.sh && \
+chmod +x $HADOOP_HOME/sbin/start-dfs.sh && \
+chmod +x $HADOOP_HOME/sbin/start-yarn.sh && \
+chmod +x ~/start-slaves.sh 
 
 /usr/local/hadoop/bin/hdfs namenode -format
